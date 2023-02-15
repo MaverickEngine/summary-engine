ca637d09eee81af6370dda05492b21caea29e6d1
Jason Norwood-Young <jason@10layer.com>
2023-02-15 14:11:36 +0100

    Better error handling if problems with OpenAI

--- includes/libs/summaryengine-openai.php
+++ includes/libs/summaryengine-openai.php
@@ -19,6 +19,22 @@ class SummaryEngineOpenAI {
         $response = wp_remote_post($url, $args);
         if (! is_wp_error( $response ) ) {
             $body = json_decode(wp_remote_retrieve_body($response), true);
+            if (is_array($body) && isset($body["error"]) && isset($body["error"]["message"])) {
+                $error_message = $body["error"]["message"];
+                // phpcs:ignore WordPress.PHP.DevelopmentFunctions
+                error_log($error_message);
+                throw new Exception("OpenAI API error: " . $error_message);
+            }
+            if (!is_array($body)) {
+                // phpcs:ignore WordPress.PHP.DevelopmentFunctions
+                error_log("No body returned from OpenAI API: " . wp_json_encode($body));
+                throw new Exception("OpenAI API error: Unexpected response from OpenAI API");
+            }
+            if (!isset($body["id"])) {
+                // phpcs:ignore WordPress.PHP.DevelopmentFunctions
+                error_log("No ID returned from OpenAI API: " . wp_json_encode($body));
+                throw new Exception("OpenAI API error: No ID returned from OpenAI API");
+            }
             return $body;
         } else {
             $error_message = $response->get_error_message();
